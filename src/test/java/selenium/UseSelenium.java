500899367374116f2e8d4fb41b80b85084cd567d
Madina Berdova <madinaberdova@Madinas-MBP.home>
2022-04-19 20:52:30 -0400

    First Push Amazon

---
+++ src/test/java/selenium/UseSelenium.java
@@ -0,0 +1,15 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class UseSelenium {
+     @Test
+    public void useSeleniumLib(){
+        System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+    }
+}
